303b3e1dba4dfbeb941600386e20ff7088e9a4d2
saijeetnik <saijeet01@gmail.com>
2025-12-10 19:19:19 +0530

    Complete the user routes

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,7 +1,12 @@
 import express from "express"
 import { userRouter } from "./router/user";
-
+const port=8080
 const app=express();
 
 app.use(express.json())
 app.use("/api/v1/user",userRouter)
+
+
+app.listen(port,()=>{
+    console.log(`app is running on port ${port}`)
+})
--- apps/server/router/user.ts
+++ apps/server/router/user.ts
@@ -1,9 +1,14 @@
-import { Router } from "express";
+import { json, Router } from "express";
 import { userSchema } from "@exeness/types";
 import { v5 } from "uuid";
 import { USERS } from "../data/db";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 export const userRouter = Router();
-userRouter.post("/signup", (req, res) => {
+const SALT_ROUND = 12;
+const SECRET="jwtsecrets"
+const NAMESPACE =process.env.USER_NAMESPACE_UUID ||"f0e1d2c3-b4a5-6789-9876-543210fedcba";
+userRouter.post("/signup", async (req, res) => {
   try {
     //get the user info
     const parseUserinfo = userSchema.safeParse(req.body);
@@ -14,18 +19,19 @@ userRouter.post("/signup", (req, res) => {
       });
     }
     const { email, password } = parseUserinfo.data;
+    //hash the user password
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUND);
     //gnerate the uuid from email
-    const id = v5(email, process.env.USER_NAMESPACE_UUID!);
-
+    const id = v5(email, NAMESPACE);
     if (USERS[id]) {
-      res.send(403).json({
-        message: "User is already exits",
+      return res.status(403).json({
+        message: "User already exists",
       });
     }
 
     USERS[id] = {
       email,
-      password,
+      password: hashedPassword,
       balance: { balance: 5000 },
     };
     return res.status(200).json({
@@ -37,3 +43,43 @@ userRouter.post("/signup", (req, res) => {
     });
   }
 });
+
+userRouter.post("/signin",async (req,res)=>{
+  try{  const parsedata=userSchema.safeParse(req.body);
+    if(!parsedata.success){
+        return res.status(403).json({
+            message:"Enter the correct credentail"
+        })
+    }
+    const {email,password}=parsedata.data
+    const id=v5(email,NAMESPACE);
+    const user=USERS[id]
+    
+    if(!user){
+       return res.status(403).json({
+            message:"User dose,t exits"
+        })
+    }
+
+    const checkpassword=await bcrypt.compare(password,user.password);
+    if(!checkpassword){
+         return res.status(403).json({
+            message:"Password is incorrect"
+        })
+   
+    }
+    const token=jwt.sign(id,SECRET)
+    return res.status(200).json({
+        message:"User sign in "
+    ,token
+    })
+    }catch{
+      return res.status(403).json({
+        message:"Server error"
+      })
+  }
+    
+})
+
+
+
