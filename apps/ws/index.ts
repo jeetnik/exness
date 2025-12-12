f7b09fd8c0195acfdd90047bd24a9d37e5522820
saijeetnik <saijeet01@gmail.com>
2025-12-13 00:49:11 +0530

    basic ws server

--- apps/ws/index.ts
+++ apps/ws/index.ts
@@ -1,14 +1,37 @@
 import { WebSocket,WebSocketServer} from "ws";
+import Redis from "ioredis";
+const redisUrl=process.env.REDIS_URL||"redis://localhost:6379"
+const subscriber=new Redis(redisUrl);
 const PORT=8080;
 const wss=new WebSocketServer({port:PORT});
 console.log(`WebSocket server running on port ${PORT}`);
+
+
+subscriber.on("message",(channel,message)=>{
+    wss.clients.forEach((client)=>{
+        if(client.readyState===WebSocket.OPEN){
+            client.send(message);
+        }
+    });
+});
+
 wss.on("connection",(ws:WebSocket)=>{
     console.log("websocket connection is establish!");
 
-    ws.on("message",(data:any)=>{
-        ws.send(data.toString())
+    ws.on("message",async (data:any)=>{
+        const parsemsg=JSON.parse(data.toString());
+        if(parsemsg.subscribe){
+            await subscriber.subscribe(parsemsg.chan);
+            ws.send(JSON.stringify(data.toString()));
+
+        }
+        if(parsemsg.unsubscribe){
+            await subscriber.unsubscribe(parsemsg.chan);
+        }
 
     })
 
-
+    ws.on("close",()=>{
+        console.log("Client disconnected");
+    });
 })
